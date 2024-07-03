a759e28e937be2b661ec5cdc68722423cc996ffb
Mohd Maroof <mdmaroof@outlook.com>
2024-07-03 19:57:17 +0530

    cors

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,9 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextResponse } from "next/server"
 import OpenAI from "openai";
 
 const openai = new OpenAI(
     { apiKey: process.env.OPEN_AI_KEY }
 );
-
 
 export const GET = async () => {
 
