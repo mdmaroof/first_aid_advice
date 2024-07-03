9ee44a14cde25d71b980352e12a746b39ac86d27
Mohd Maroof <mdmaroof@outlook.com>
2024-07-03 20:15:50 +0530

    flex

--- components/homePage/searchInput.js
+++ components/homePage/searchInput.js
@@ -35,9 +35,9 @@ const InputSearchBox = ({ onClick, input, setInput }) => {
                 <input
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
-                    className="text-lg flex-1 bg-transparent focus-visible:outline-0 pl-2 pr-1" />
+                    className="text-lg flex1 bg-transparent focus-visible:outline-0 pl-2 pr-1" />
                 <div
-                    className="bg-[#ff7b73] flex-1 text-white py-2 px-4 rounded-full font-bold"
+                    className="bg-[#ff7b73] text-white py-2 px-4 rounded-full font-bold"
                     onClick={onClick}
                 >
                     Search
