3233f6c97794b00896ec594283c13be98473c5b3
Mohd Maroof <mdmaroof@outlook.com>
2024-07-03 20:20:12 +0530

    center search

--- components/homePage/searchInput.js
+++ components/homePage/searchInput.js
@@ -37,7 +37,7 @@ const InputSearchBox = ({ onClick, input, setInput }) => {
                     onChange={(e) => setInput(e.target.value)}
                     className="text-lg col-span-2 bg-transparent focus-visible:outline-0 pl-2 pr-1" />
                 <div
-                    className="bg-[#ff7b73] text-white py-2 px-4 rounded-full font-bold"
+                    className="bg-[#ff7b73] text-white py-2 px-4 rounded-full font-bold flex items-center justify-center"
                     onClick={onClick}
                 >
                     Search
