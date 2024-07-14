3ee2169cadb6c06597cfc72a009d86e464ca2382
Mohd Maroof <mdmaroof@outlook.com>
2024-07-14 18:15:03 +0530

    mobile view

--- components/homePage/title.js
+++ components/homePage/title.js
@@ -1,4 +1,4 @@
-export const Title = ({ size = "w-[250px] h-[250px]", textSize = "text-5xl" }) => {
+export const Title = ({ size = "w-[175px] h-[175px] md:w-[250px] md:h-[250px]", textSize = "text-2xl md:text-5xl" }) => {
     return (
         <div className={`flex justify-center items-center bg-white ${size} rounded-full`} >
             <div className={`${textSize} font-quicksand text-[#ff7b73]`}>SnapAid</div>
