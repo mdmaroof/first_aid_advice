4947d55beed23fa5172b75ee53d418964293da9e
Mohd Maroof <mdmaroof@outlook.com>
2024-07-14 15:55:56 +0530

    title logo

--- components/homePage/title.js
+++ components/homePage/title.js
@@ -1,7 +1,7 @@
 export const Title = () => {
     return (
-        <div className="flex justify-center items-center">
-            <div className="text-5xl font-quicksand">SnapAid</div>
+        <div className="flex justify-center items-center bg-white w-[250px] h-[250px] rounded-full">
+            <div className="text-5xl font-quicksand text-[#ff7b73]">SnapAid</div>
         </div>
     )
 }
